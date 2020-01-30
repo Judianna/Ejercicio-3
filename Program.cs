d6f861c200a5312e79a679c452c1af240fabf912
Judianna <58674281+Judianna@users.noreply.github.com>
2020-01-30 20:12:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {  
+            List<int> multiplos = new List<int>();
+            Console.WriteLine("Indique un número");
+            int m = Convert.ToInt16(Console.ReadLine());
+            for (int i=1; m >= multiplos.Count; i++)
+            
+            {
+                if (i % 3 ==0)
+                {
+                if (i % 5 != 0)
+                {
+                    multiplos.Add(i);
+                }
+                }
+            }
+            foreach(var num in multiplos)
+            {
+                Console.WriteLine("{0}",num);
+            }
+
+
+
+        }
+    }
+}
